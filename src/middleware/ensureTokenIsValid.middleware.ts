6ddaf345b4ff80b09279ed8a2d69f651c4e62dd5
Lucas Wesley Fragoso <lucaswmmf@gmail.com>
2023-03-02 18:15:42 -0300

    add- listUsers route

---
+++ src/middleware/ensureTokenIsValid.middleware.ts
@@ -0,0 +1,28 @@
+import { Request,Response, NextFunction } from "express";
+import { AppError } from '../errors';
+import jwt from 'jsonwebtoken';
+
+const ensureTokenIsValidMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    let token = req.headers.authorization
+
+    if(!token){
+        throw new AppError('Token is missing', 401)
+    }
+
+    token = token.split(' ')[1]
+
+    jwt.verify(token, process.env.SECRET_KEY!, (error, decoded: any) => {
+
+        if(error){
+            throw new AppError(error.message, 401)
+        }
+
+        req.user = {
+            isAdmin: decoded.isAdmin
+        }
+
+        next()
+    })
+}
+
+export default ensureTokenIsValidMiddleware
--- src/routers/users.routes.ts
+++ src/routers/users.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import { createUserController } from "../controllers/users.controllers";
+import { createUserController, listUsersController } from "../controllers/users.controllers";
 import ensureDataIsValidMiddleware from "../middleware/ensureDataIsValid.middleware";
+import ensureTokenIsValidMiddleware from "../middleware/ensureTokenIsValid.middleware";
 import verifyEmailExistMiddleware from "../middleware/verifyEmailExist.middleware";
 import { userDataRequest } from "../schemas/users.schema";
 
 const usersRoutes: Router = Router()
 
 usersRoutes.post('', verifyEmailExistMiddleware, ensureDataIsValidMiddleware(userDataRequest), createUserController)
+usersRoutes.get('', ensureTokenIsValidMiddleware, listUsersController)
 
 
 export {
